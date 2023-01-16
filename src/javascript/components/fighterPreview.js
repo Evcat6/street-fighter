741a1fe5f5bd9a78f2ac19ec2dcf347e9bcef407
unknown <demomusicevcat@gmail.com>
2023-01-16 18:58:20 +0100

    created logic of choosing fighter

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,7 +7,44 @@ export function createFighterPreview(fighter, position) {
     className: `fighter-preview___root ${positionClassName}`,
   });
 
-  // todo: show fighter info (image, name, health, etc.)
+  
+  function createFighterPhoto(image) {
+    const element = createElement({
+      tagName: 'img',
+      className: 'fighter-preview___image',
+      attributes: {
+        src: image
+      }
+    });
+    
+    
+    if(position === 'right') {
+      element.style.transform = 'scale(-1, 1)';
+    }
+
+    position === 'right'?  element.style.transform = 'scale(-1, 1)': null;
+    return element;
+  }
+  
+  function createFighterInfoCloumn(fighter) {
+    const fighterInfoItem = createElement({
+      tagName: 'span',
+      className: 'fighter-preview___info-item'
+    });
+    fighterInfoItem.innerText = fighter;
+    return fighterInfoItem;
+  }
+  
+  
+  if(fighter) {
+    const fighterImage = createFighterPhoto(fighter['source']);
+    const keyValueArray = Object.entries(fighter);
+    fighterElement.append(fighterImage);
+    keyValueArray
+      .filter(e => e[0] !== '_id' && e[0] !== 'source')
+      .map(el => fighterElement.append(createFighterInfoCloumn(el.join(': '))));
+  }
+
 
   return fighterElement;
 }
